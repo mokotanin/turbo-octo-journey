233e83a249e46fc882adb0fc3b1c11d0b36f780a
Tristan <titchou68@gmail.com>
2025-08-10 02:43:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ export default {
       {
         id: "naruto",
         title: "Naruto",
-        image: "https://raw.githubusercontent.com/TonPseudo/ton-repo/main/images/naruto.jpg",
+        image: "https://raw.githubusercontent.com/mokotanin/turbo-octo-journey/refs/heads/main/naruto.jpg",
         url: "https://anime-sama.fr/catalogue/naruto/"
       },
       {
         id: "onepiece",
         title: "One Piece",
-        image: "https://raw.githubusercontent.com/TonPseudo/ton-repo/main/images/onepiece.jpg",
+        image: "https://raw.githubusercontent.com/mokotanin/turbo-octo-journey/refs/heads/main/onepiece.jpg",
         url: "https://onepace.net/fr/watch"
       }
     ];
