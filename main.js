62792e2b51a52ac892cba162cf725dd46ccc65f3
Tristan <titchou68@gmail.com>
2025-08-10 02:38:59 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+export default {
+  async list() {
+    return [
+      {
+        id: "naruto",
+        title: "Naruto",
+        image: "https://raw.githubusercontent.com/TonPseudo/ton-repo/main/images/naruto.jpg",
+        url: "https://anime-sama.fr/catalogue/naruto/"
+      },
+      {
+        id: "onepiece",
+        title: "One Piece",
+        image: "https://raw.githubusercontent.com/TonPseudo/ton-repo/main/images/onepiece.jpg",
+        url: "https://onepace.net/fr/watch"
+      }
+    ];
+  }
+}
